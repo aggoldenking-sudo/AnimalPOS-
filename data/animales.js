b6d580d3c2cbcc7aa25c0f83275ea3f689d6f809
aggoldenking-sudo <aggoldenking@gmail.com>
2026-01-05 23:06:25 -0400

    Create animales.js

---
+++ data/animales.js
@@ -0,0 +1,24 @@
+const animales = [
+  "🐬 Delfín","🐋 Ballena","🐏 Carnero","🐂 Toro","🐛 Ciempiés","🦂 Alacrán","🦁 León",
+  "🐸 Rana","🦜 Perico","🐭 Ratón","🦅 Águila","🐯 Tigre","🐱 Gato","🐴 Caballo",
+  "🐵 Mono","🕊 Paloma","🦊 Zorro","🐻 Oso","🦚 Pavo","🫏 Burro","🐐 Chivo",
+  "🐖 Cochino","🐓 Gallo","🐫 Camello","🦓 Cebra","🦎 Iguana","🐔 Gallina",
+  "🐄 Vaca","🐕 Perro","🐘 Elefante","🐊 Caimán","🐌 Lapa","🐿 Ardilla",
+  "🐟 Pescado","🦌 Venado","🦒 Jirafa","🐍 Culebra"
+];
+
+const animalesMap = [
+  {num:1,nombre:"Delfín"},{num:2,nombre:"Ballena"},{num:3,nombre:"Carnero"},
+  {num:4,nombre:"Toro"},{num:5,nombre:"Ciempiés"},{num:6,nombre:"Alacrán"},
+  {num:7,nombre:"León"},{num:8,nombre:"Rana"},{num:9,nombre:"Perico"},
+  {num:10,nombre:"Ratón"},{num:11,nombre:"Águila"},{num:12,nombre:"Tigre"},
+  {num:13,nombre:"Gato"},{num:14,nombre:"Caballo"},{num:15,nombre:"Mono"},
+  {num:16,nombre:"Paloma"},{num:17,nombre:"Zorro"},{num:18,nombre:"Oso"},
+  {num:19,nombre:"Pavo"},{num:20,nombre:"Burro"},{num:21,nombre:"Chivo"},
+  {num:22,nombre:"Cochino"},{num:23,nombre:"Gallo"},{num:24,nombre:"Camello"},
+  {num:25,nombre:"Cebra"},{num:26,nombre:"Iguana"},{num:27,nombre:"Gallina"},
+  {num:28,nombre:"Vaca"},{num:29,nombre:"Perro"},{num:30,nombre:"Elefante"},
+  {num:31,nombre:"Caimán"},{num:32,nombre:"Lapa"},{num:33,nombre:"Ardilla"},
+  {num:34,nombre:"Pescado"},{num:35,nombre:"Venado"},{num:36,nombre:"Jirafa"},
+  {num:37,nombre:"Culebra"}
+];
