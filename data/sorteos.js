50a8a28a34e06d96cf5ee448fcc72122057e8cbd
aggoldenking-sudo <aggoldenking@gmail.com>
2026-01-06 00:49:04 -0400

    Update sorteos.js

--- data/sorteos.js
+++ data/sorteos.js
@@ -1,4 +1,36 @@
 const sorteos = [
-  {nombre: "Lotto Activo", horarios: ["8AM","9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM","6PM","7PM"]},
-  {nombre: "La Granjita", horarios: ["8AM","9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM","6PM","7PM"]}
+  {
+    nombre: "Lotto Activo",
+    horarios: [
+      "08:00 AM",
+      "09:00 AM",
+      "10:00 AM",
+      "11:00 AM",
+      "12:00 PM",
+      "01:00 PM",
+      "02:00 PM",
+      "03:00 PM",
+      "04:00 PM",
+      "05:00 PM",
+      "06:00 PM",
+      "07:00 PM"
+    ]
+  },
+  {
+    nombre: "La Granjita",
+    horarios: [
+      "08:00 AM",
+      "09:00 AM",
+      "10:00 AM",
+      "11:00 AM",
+      "12:00 PM",
+      "01:00 PM",
+      "02:00 PM",
+      "03:00 PM",
+      "04:00 PM",
+      "05:00 PM",
+      "06:00 PM",
+      "07:00 PM"
+    ]
+  }
 ];
