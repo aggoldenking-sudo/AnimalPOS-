f26f48c05c0b2cc943720eed7a21f478804ddd45
aggoldenking-sudo <aggoldenking@gmail.com>
2026-01-05 23:07:12 -0400

    Create sorteos.js

---
+++ data/sorteos.js
@@ -0,0 +1,4 @@
+const sorteos = [
+  {nombre: "Lotto Activo", horarios: ["8AM","9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM","6PM","7PM"]},
+  {nombre: "La Granjita", horarios: ["8AM","9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM","6PM","7PM"]}
+];
