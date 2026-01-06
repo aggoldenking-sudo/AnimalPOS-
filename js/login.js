99d95023980266971a573db0b8526a61967141de
aggoldenking-sudo <aggoldenking@gmail.com>
2026-01-05 23:08:09 -0400

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,20 @@
+const USUARIO = "Golden King";
+const CLAVE = "1234";
+
+function login() {
+  const usuario = document.getElementById("usuarioInput").value.trim();
+  const clave = document.getElementById("claveInput").value.trim();
+  if (usuario !== USUARIO || clave !== CLAVE) return alert("Usuario o clave incorrecta");
+
+  document.getElementById("loginScreen").style.display = "none";
+  document.getElementById("mainScreen").style.display = "block";
+  document.getElementById("userTicket").innerText = usuario;
+  document.getElementById("userLabel").innerText = "Usuario: " + usuario;
+}
+
+function salir() {
+  document.getElementById("mainScreen").style.display = "none";
+  document.getElementById("loginScreen").style.display = "block";
+  document.getElementById("usuarioInput").value = "";
+  document.getElementById("claveInput").value = "";
+}
